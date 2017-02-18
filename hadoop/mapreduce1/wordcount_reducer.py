4ea3715f78d711610584830f9dbf584f0ded13d0
JoanPinol <joan.pinol@gmail.com>
2017-02-18 13:36:33 +0100

    Update wordcount_reducer.py

--- hadoop/mapreduce1/wordcount_reducer.py
+++ hadoop/mapreduce1/wordcount_reducer.py
@@ -8,12 +8,11 @@ running_total = 0
 for input_line in sys.stdin:
     input_line = input_line.strip()
 
-    # Get Next Word    # --------------------------------
     this_key, value = input_line.split("\t", 1)  #the Hadoop default is tab separates key value
                           #the split command returns a list of strings, in this case into 2 variables
     value = int(value)           #int() will convert a string to integer (this program does no error checking)
  
-    if last_key == this_key:     #check if key has changed ('==' is                                   #      logical equalilty check
+    if last_key == this_key:     #check if key has changed ('==' is logical equalilty check
         running_total += value   # add value to running total
 
     else:
