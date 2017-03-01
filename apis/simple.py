e9cac73321c183b4846f696724d5f315604ea9dd
JoanPinol <joan.pinol@gmail.com>
2017-03-01 13:46:39 +0100

    Update simple.py

--- apis/simple.py
+++ apis/simple.py
@@ -1,3 +1,5 @@
+import requests
+
 # Make a get request to get the latest position of the international space station from the opennotify api.
 response = requests.get("http://api.open-notify.org/iss-now.json")
 
